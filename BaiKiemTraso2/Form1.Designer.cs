43e3ee40ae2086d6d6d6a635937ba3bb8272256e
HP <HP@DESKTOP-CO09DFI>
2024-10-31 16:32:04 +0700

    Add project files.

---
+++ BaiKiemTraso2/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace BaiKiemTraso2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonThem = new System.Windows.Forms.Button();
+            this.buttonXoa = new System.Windows.Forms.Button();
+            this.buttonThanhtoan = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(130, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Thông tin sản phẩm";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(270, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(250, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ứng dụng bán hàng";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(301, 158);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Tìm kiếm";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // buttonThem
+            // 
+            this.buttonThem.Location = new System.Drawing.Point(202, 379);
+            this.buttonThem.Name = "buttonThem";
+            this.buttonThem.Size = new System.Drawing.Size(174, 23);
+            this.buttonThem.TabIndex = 4;
+            this.buttonThem.Text = "Thêm vào giỏ hàng";
+            this.buttonThem.UseVisualStyleBackColor = true;
+            this.buttonThem.Click += new System.EventHandler(this.buttonThem_Click);
+            // 
+            // buttonXoa
+            // 
+            this.buttonXoa.Location = new System.Drawing.Point(440, 379);
+            this.buttonXoa.Name = "buttonXoa";
+            this.buttonXoa.Size = new System.Drawing.Size(75, 23);
+            this.buttonXoa.TabIndex = 5;
+            this.buttonXoa.Text = "Xóa khỏi giỏ hàng";
+            this.buttonXoa.UseVisualStyleBackColor = true;
+            this.buttonXoa.Click += new System.EventHandler(this.buttonXoa_Click);
+            // 
+            // buttonThanhtoan
+            // 
+            this.buttonThanhtoan.Location = new System.Drawing.Point(686, 379);
+            this.buttonThanhtoan.Name = "buttonThanhtoan";
+            this.buttonThanhtoan.Size = new System.Drawing.Size(75, 23);
+            this.buttonThanhtoan.TabIndex = 6;
+            this.buttonThanhtoan.Text = "Thanh toán";
+            this.buttonThanhtoan.UseVisualStyleBackColor = true;
+            this.buttonThanhtoan.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(617, 384);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "label3";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(43, 187);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(333, 186);
+            this.listView1.TabIndex = 9;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // listView2
+            // 
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(440, 187);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(310, 186);
+            this.listView2.TabIndex = 10;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonThanhtoan);
+            this.Controls.Add(this.buttonXoa);
+            this.Controls.Add(this.buttonThem);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonThem;
+        private System.Windows.Forms.Button buttonXoa;
+        private System.Windows.Forms.Button buttonThanhtoan;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView listView2;
+    }
+}
+
---
+++ BaiKiemTraso2/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaiKiemTraso2
+{
+    public partial class Form1 : Form
+    {
+        private List<Sanpham> items = new List<Sanpham>();
+        private Giohang cart = new Giohang();
+        public class Sanpham
+        {
+            public string tensp { get; set; }
+            public decimal gia { get; set; }
+            public int soluong { get; set; }
+            public Sanpham(string name, decimal price, int sl)
+            {
+                tensp = name;
+                gia = price;
+                soluong = sl;
+            }
+        }
+        public class Giohang
+        {
+            private List<Sanpham> items = new List<Sanpham>();
+            public void Themsp(Sanpham sanpham)
+            {
+                items.Add(sanpham);
+            }
+            public void Xoasp(Sanpham sanpham)
+            {
+                items.Remove(sanpham);
+            }
+            public decimal Tinhtong(Sanpham sanpham)
+            {
+                return items.Sum(p => p.gia * p.soluong);
+            }
+            public List<Sanpham> GetItems()
+            {
+                return items;
+            }
+            public void Clear()
+            {
+                items.Clear();
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            capnhatdanhsach();
+           // capnhatgia();
+            chonsanphamtudanhsach();
+        }
+        private void sanpham()
+        {
+            items.Add(new Sanpham("sp1", 1500, 1));
+            items.Add(new Sanpham("sp2", 100, 5));
+            items.Add(new Sanpham("sp3", 2500, 11));
+            items.Add(new Sanpham("sp4", 3500, 17));
+            items.Add(new Sanpham("sp5", 5000, 16));
+            items.Add(new Sanpham("sp6", 10000, 19));
+        }
+        private void capnhatdanhsach()
+        {
+            listView1.Items.Clear();
+            foreach (var product in items)
+            {
+                ListViewItem item = new ListViewItem(product.tensp);
+                item.SubItems.Add(product.gia.ToString("C"));
+                item.SubItems.Add(product.soluong.ToString());
+                listView1.Items.Add(item);
+            }
+        }
+        private Sanpham chonsanphamtudanhsach()
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                var selecteditem = listView1.SelectedItems[0];
+                string name = selecteditem.SubItems[0].Text;
+                decimal price = decimal.Parse(selecteditem.SubItems[1].Text, System.Globalization.NumberStyles.Currency);
+                int sl = int.Parse(selecteditem.SubItems[2].Text);
+                return new Sanpham(name, price, sl);
+            }
+            return null;
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("thanh toan thanh cong");
+            cart.GetItems();
+            capnhatdanhsach();
+            capnhatgia();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonThem_Click(object sender, EventArgs e)
+        {
+            listView2.Items.Clear();
+            foreach (var item in cart.GetItems())
+            {
+                var listitem = new ListViewItem(item.tensp);
+                listitem.SubItems.Add(item.gia.ToString("C"));
+                listitem.SubItems.Add(item.soluong.ToString());
+                listView2.Items.Add(listitem);
+            }
+        }
+        
+        private void capnhatgia()
+        {
+            //label3.Text = $"Tong gia tri don hang: {cart.Tinhtong():C}";
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void buttonXoa_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
